d72144c97a5d028e93341dd1d69cf4ec4c22f0cf
Shan <shantanu@zantrik.in>
2018-06-29 17:27:26 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'gauhati-high-court-app',
+		wp_ws_url : 'http://web3soft.org/ghcfinal/wp-appkit-api/gauhati-high-court-app',
+		wp_url : 'http://web3soft.org/ghcfinal',
+		theme : 'q-android',
+		version : '',
+		app_type : 'phonegap-build',
+		app_title : 'Gauhati High Court',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'HD*HE=isDF{u#62OffoWRScL ]eS]_(sby.l8aE-Wu<[BH<]uVX&[o=N2]g#ejp2',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
